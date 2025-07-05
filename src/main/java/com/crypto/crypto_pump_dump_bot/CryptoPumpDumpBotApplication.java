89d3fe9be0c0bab9e5b60b4152b612eec4d5dc56
grigorii_pokatilo <zilibobin.tik.tok@gmail.com>
2025-07-05 14:31:13 +0300

    Spring Boot project setup

---
+++ src/main/java/com/crypto/crypto_pump_dump_bot/CryptoPumpDumpBotApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.crypto_pump_dump_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPumpDumpBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPumpDumpBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/crypto_pump_dump_bot/CryptoPumpDumpBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.crypto_pump_dump_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPumpDumpBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
